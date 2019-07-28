6386c74da79575fb83d43ffa74ae34197549d5e1
lsj <1004624379@qq.com>
2019-07-29 00:30:34 +0800

    第八次 规范格式

--- LabelBinarizer.py
+++
@@ -1,6 +0,0 @@
-from sklearn.preprocessing import LabelBinarizer
-
-y = [0,1,1,2]
-y = LabelBinarizer().fit_transform(y)
-
-print(y)
--- MultiLabelBinarizer.py
+++
@@ -1,9 +0,0 @@
-from sklearn.preprocessing import MultiLabelBinarizer
-
-mlb = MultiLabelBinarizer()
-print(mlb .fit_transform([(1, 2), (3,),(4,),(1,3)]))
-print(mlb)
-print(mlb.classes_)
-
-print(mlb.fit_transform([{'python','C++'},{'JAVA'}]))
-print(mlb.classes_)
